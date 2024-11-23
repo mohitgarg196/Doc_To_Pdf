e8460fade87c32b8f715bef1fec849da76517674
Mohit Garg <mohitgarg8910@gmail.com>
2024-11-23 22:54:22 +0530

    docker commit

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ app.get("/view", (req, res) => {
   `);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
